e5b006828807ddba3457bf3f1318ab6fda9e048c
Franc Siswanto <fr.siswanto@gmail.com>
2021-03-23 10:29:12 +0700

    Update TestController.php

--- TestController.php
+++ TestController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Support\Facades\DB;
 use DB;
 use Illuminate\Http\Request;
 
@@ -18,7 +17,6 @@ class TestController extends Controller {
 		$lastUpl3 = [];
 		$lastUpl4 = [];
 		$lastUpl5 = [];
-
 
 		$ii = 0;
 		foreach ($data as $key => $value) {
@@ -51,78 +49,8 @@ class TestController extends Controller {
 			}
 		}
 
-		return response()->json($arMenu, 200);
+		return response()->json($arMenu);
 
-	}
-
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id) {
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id) {
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  int                      $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id) {
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id) {
-		//
 	}
 
 }
